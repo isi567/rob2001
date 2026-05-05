420ae26a5b742ea2bcbaf67777b1f0bc7bf18cfe
yazz3660 <yazz.alderson@gmail.com>
2026-05-05 09:48:47 +0100

    IP address added

--- CafeRobot/roboTextServer.py
+++ CafeRobot/roboTextServer.py
@@ -19,7 +19,9 @@ import threading
 
 # define the IP address of the machine on which the server is running.
 # this could be a dotted quad address or 'localhost' (shorthand for the local machine).
-HOST = 'localhost'   # or, e.g. '10.5.24.62'
+
+#HOST = 'localhost'   # or, e.g. '10.5.24.62'
+HOST = '10.247.72.18'
 
 # define the port for clients to connect to this socket.
 # the value could be almost anything--that isn't already in use.
