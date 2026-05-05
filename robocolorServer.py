b871df2e79b5ea3d28517d2b143ccbd260dd58cb
yazz3660 <yazz.alderson@gmail.com>
2026-05-05 09:19:29 +0100

    update

--- robocolorServer.py
+++ robocolorServer.py
@@ -28,6 +28,7 @@ PORT = 50007
 # define messages
 MSG_REGISTER = 'REGISTER'
 MSG_COLOUR = 'GREEN'
+MSG_RECEIVED = 'RECEIVED'
 
 # define server state variables
 STATE_SERVER_STARTING = 0
@@ -43,6 +44,7 @@ STATE_CLIENT_RECEIVE_COLOUR = 4
 STATE_CLIENT_SEND_COLOUR    = 5
 
 ClientList = []
+ClientListLock = threading.Lock()
 
 #--
 # define thread for handling communication with a single client.
@@ -64,24 +66,49 @@ class client_thread( threading.Thread ):
     def run( self ):
         self.setState( STATE_CLIENT_RUNNING )
         print( '[server client %s]: running' % ( self.name ))
+        buffer = ''
         while True:
             print( '[server client %s]: waiting for message...' % ( self.name ))
-            client_msg = self.connection.recv( 1024 ) # receive message from client
-            print( '[server client %s]: message received: %s' % ( self.name, client_msg.decode() ))
-            if not client_msg:
+            try:
+                chunk = self.connection.recv( 1024 ) # receive message from client
+            except:
+                chunk = b''
+            if not chunk:
                 # if an empty message is received, then the client has disconnected; so exit.
                 self.setState( STATE_CLIENT_EXITING )
                 break
-            else:
-                client_msg_tokens = client_msg.decode().split()
+
+            buffer += chunk.decode()
+            lines = buffer.split( '\n' )
+            buffer = lines[-1]  # keep incomplete line in buffer
+            
+            for line in lines[:-1]:
+                if not line.strip():
+                    continue
+                decoded_msg = line.strip()
+                print( '[server client %s]: message received: %s' % ( self.name, decoded_msg ))
+                client_msg_tokens = decoded_msg.split()
                 if ( client_msg_tokens[0] == MSG_REGISTER ):
                     self.name = client_msg_tokens[1]
-                elif ( client_msg_tokens[0] == MSG_COLOUR ):
-                    server_msg = 'Server has received ' + MSG_COLOUR + ' from ' + self.name
+                elif ( client_msg_tokens[0] == MSG_COLOUR or client_msg_tokens[0] == MSG_RECEIVED ):
+                    msg_type = client_msg_tokens[0]
+                    msg_payload = ' '.join( client_msg_tokens[3:] )
+                    server_msg = 'Server has received ' + msg_type + ' from ' + self.name
                     print( '[server client %s]: sending message [%s]' % ( self.name, server_msg ))
-                    self.connection.sendall( server_msg.encode() ) # send message to client
-                    #self.setState(STATE_CLIENT_SEND_COLOUR)
-                    
+                    self.connection.sendall( (server_msg + '\n').encode() ) # send message to client
+                    forwarded_msg = 'Forwarded ' + msg_type + ' from ' + self.name
+                    if ( msg_payload ):
+                        forwarded_msg += ' ' + msg_payload
+                    forwarded = False
+                    with ClientListLock:
+                        for other_client in ClientList:
+                            if other_client is not self and other_client.state == STATE_CLIENT_RUNNING:
+                                print( '[server client %s]: forwarding message [%s] to [%s]' % ( self.name, forwarded_msg, other_client.name ))
+                                other_client.connection.sendall( (forwarded_msg + '\n').encode() )
+                                forwarded = True
+                                break
+                    if not forwarded:
+                        print( '[server client %s]: no second client available for forwarding' % ( self.name ))
 
 
         self.setState( STATE_CLIENT_EXITING )
@@ -116,9 +143,11 @@ with socket.socket( socket.AF_INET, socket.SOCK_STREAM ) as ss:
         # now loop forever, reading messages on the socket and sending back acknowledgments.
         name = str( addr )
         print( '[server main] creating robot client thread named: [%s]' % ( name ))
-        my_clients.append( client_thread( name, connection ))
-        my_clients[-1].start()
-        my_clients[-1].setState( STATE_CLIENT_RUNNING )
+        new_client = client_thread( name, connection )
+        with ClientListLock:
+            ClientList.append( new_client )
+        new_client.start()
+        new_client.setState( STATE_CLIENT_RUNNING )
 
 server_state = STATE_SERVER_EXITING
 print( '[server main] exiting' )
